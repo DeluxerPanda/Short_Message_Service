263cb275a32b0bfd62b2457606d283122d11adba
DeluxPanda <github@deluxerpanda.se>
2023-10-31 10:59:29 +0100

    update

--- app/src/main/java/se/deluxerpanda/smssender/MainActivity.java
+++ app/src/main/java/se/deluxerpanda/smssender/MainActivity.java
@@ -1,11 +1,12 @@
-package se.deluxerpanda.smssender;// MainActivity.java
+package se.deluxerpanda.smssender;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -14,6 +15,14 @@ import androidx.core.content.ContextCompat;
 public class MainActivity extends AppCompatActivity {
     private static final int SMS_PERMISSION_REQUEST_CODE = 1;
     private EditText phoneNumberEditText, messageEditText;
+    private boolean hasSendSmsPermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS);
+        return permissionCheck == PackageManager.PERMISSION_GRANTED;
+    }
+    private void requestSendSmsPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS},
+                SMS_PERMISSION_REQUEST_CODE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +36,15 @@ public class MainActivity extends AppCompatActivity {
         sendButton.setOnClickListener(view -> {
             String phoneNumber = phoneNumberEditText.getText().toString();
             String message = messageEditText.getText().toString();
-            sendSMS(phoneNumber, message);
+            if (hasSendSmsPermission()) {
+                if (!phoneNumber.isEmpty() && !message.isEmpty()) {
+                    sendSMS(phoneNumber, message);
+                } else {
+                    showMessage("Please fill in both phone number and message fields.");
+                }
+            } else {
+                requestSendSmsPermission();
+            }
         });
 
 
@@ -40,8 +57,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sendSMS(String phoneNumber, String message) {
-        SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNumber, null, message, null, null);
     }
 
     @Override
@@ -51,9 +68,14 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == SMS_PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Permission granted
+                showMessage("Grate you have permission");
             } else {
                 // Permission denied
+                showMessage("Please grant SMS permission to send messages.");
             }
         }
+    }
+    private void showMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
