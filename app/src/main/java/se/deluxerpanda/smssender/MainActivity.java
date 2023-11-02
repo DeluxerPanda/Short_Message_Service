d0127553b302d9fbc3df8136d03b8aa07fd45f8d
DeluxPanda <github@deluxerpanda.se>
2023-11-02 09:52:15 +0100

    update

--- app/src/main/java/se/deluxerpanda/smssender/MainActivity.java
+++ app/src/main/java/se/deluxerpanda/smssender/MainActivity.java
@@ -1,16 +1,24 @@
 package se.deluxerpanda.smssender;
 import android.Manifest;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private static final int SMS_PERMISSION_REQUEST_CODE = 1;
@@ -30,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView((R.layout.activity_main));
 
         phoneNumberEditText = findViewById(R.id.phoneNumberEditText);
-        messageEditText = findViewById(R.id.timeEditText);
+        messageEditText = findViewById(R.id.messageEditText);
 
         Button sendButton = findViewById(R.id.sendB);
         sendButton.setOnClickListener(view -> {
@@ -54,12 +62,66 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS},
                     SMS_PERMISSION_REQUEST_CODE);
         }
+        Button pickTimeButton = findViewById(R.id.pickTime);
+        pickTimeButton.setOnClickListener(view -> {
+            TimePickerFragment timePickerFragment = new TimePickerFragment();
+            timePickerFragment.show(getSupportFragmentManager(), "timePicker");
+        });
+
+        Button pickDateButton = findViewById(R.id.pickDate);
+        pickDateButton.setOnClickListener(view -> {
+                DatePickerFragment datePickerFragment = new DatePickerFragment();
+                datePickerFragment.show(getSupportFragmentManager(), "datePicker");
+        });
+
+
+
     }
 
-    private void sendSMS(String phoneNumber, String message) {
-        try {
+    //time start
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker.
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
 
-        }catch ()
+            // Create a new instance of TimePickerDialog and return it with 24-hour format.
+            return new TimePickerDialog(getActivity(), this, hour, minute, true);
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time the user picks.
+        }
+    }
+//time ends
+//date start
+public static class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker.
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it.
+        return new DatePickerDialog(requireContext(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        // Do something with the date the user picks.
+    }
+}
+// data ends
+
+
+    private void sendSMS(String phoneNumber, String message) {
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(phoneNumber, null, message, null, null);
         showMessage("SMS sent");
