62213a6ca38cdfec71c881e92091acf0a1726549
DeluxerPanda <85413771+DeluxerPanda@users.noreply.github.com>
2024-08-27 09:21:08 +0200

    update versionName to 1.6.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 27
         targetSdk = 34
         versionCode = 1
-        versionName = "1.6.3"
+        versionName = "1.6.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
